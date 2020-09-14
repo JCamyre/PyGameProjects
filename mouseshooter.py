7ea287d82fdffd49d27e49539f88b20fae44b6af
JCamyre <JWcamry03@gmail.com>
2020-09-14 12:38:43 -0700

    Fixed the camera and player movement, added an Enemy class, and fixed gun direction.

--- mouseshooter.py
+++ mouseshooter.py
@@ -1,5 +1,5 @@
 import pygame, sys
-from math import asin, sin, cos, pi
+from math import asin, sin, cos, pi, degrees
 mainClock = pygame.time.Clock()
 from pygame.locals import *
 
@@ -28,13 +28,11 @@ char_front.set_colorkey((255, 255, 255))
 char_behind = pygame.image.load('char_behind.png').convert()
 char_behind.set_colorkey((255, 255, 255))
 
-target = pygame.Rect(400, 400, 100, 100)
-
 player_rect = pygame.Rect(600, 400, 40, 104)
 
 def player_mouse_direction():
 	mx, my = pygame.mouse.get_pos()
-	x, y = player_rect.x, player_rect.y+25 
+	x, y = player_rect.x - scroll[0], player_rect.y+25 - scroll[1]
 	hypotenuse = ((mx - x)**2 + (my - y)**2)**0.5
 	height = my - y
 	direction = asin(height/hypotenuse)
@@ -43,68 +41,110 @@ def player_mouse_direction():
 class Bullet:
 
 	def __init__(self):
-		self.x = player_rect.x
-		self.y = player_rect.y
+		self.rect = pygame.Rect(player_rect.x, player_rect.y, player_rect.width, player_rect.height)
 		self.direction = player_mouse_direction()
 		self.vel_x = 0
 		self.vel_y = 0
+		self.flip = player_flip
 		self.get_velocity(10)
+		self.count = 0
 
 	def get_velocity(self, hypotenuse):
-		if player_flip:
+		if self.flip:
 			self.vel_x = -1 * hypotenuse * cos(self.direction)
 		else:
 			self.vel_x = hypotenuse * cos(self.direction)
 		self.vel_y = hypotenuse * sin(self.direction)
 
+	def move(self):
+		self.rect.x += self.vel_x
+		self.rect.y += self.vel_y
+
 
 bullet_img = pygame.image.load('bullet.png').convert()
+bullet_img = pygame.transform.scale(bullet_img, (36, 16))
 bullet_img.set_colorkey((255, 255, 255))
 
 bullets = []
+
+class Enemy:
+
+	def __init__(self):
+		self.rect = pygame.Rect(800, 400, 50, 50)
+		self.image = pygame.image.load('data/images/entities/enemy/idle/idle_0.png').convert()
+		self.image.set_colorkey((255, 255, 255))
+
+enemies = []
+enemy1 = Enemy()
+enemies.append(enemy1)
 
 clicking = False
 
 player_flip = False
 
+moving_right = False
+moving_left = False
+moving_forward = False
+moving_backward = False
+
+true_scroll = [0, 0]
+
 while True:
 	screen.fill((0, 120, 0))
 
+	true_scroll[0] += (player_rect.x-true_scroll[0]-600)/20
+	true_scroll[1] += (player_rect.y-true_scroll[1]-400)/20
+	scroll = true_scroll.copy()
+	scroll[0] = int(scroll[0])
+	scroll[1] = int(scroll[1])
+
+	# Mouse aiming
 	mx, my = pygame.mouse.get_pos()
 
-	if mx > 600:
+	if mx > player_rect.x - scroll[0]:
 		player_flip = False
 	else:
 		player_flip = True
 
-	if 400 < my:
-		screen.blit(pygame.transform.flip(pygame.transform.scale(char_front, (40, 104)), player_flip, False), (600, 400))
+	if player_rect.y - scroll[1] < my:
+		screen.blit(pygame.transform.flip(pygame.transform.scale(char_front, (20, 52)), player_flip, False), (player_rect.x - scroll[0], player_rect.y - scroll[1]))
 	else:
-		screen.blit(pygame.transform.flip(pygame.transform.scale(char_behind, (40, 104)), player_flip, False), (600, 400))
+		screen.blit(pygame.transform.flip(pygame.transform.scale(char_behind, (20, 52)), player_flip, False), (player_rect.x - scroll[0], player_rect.y - scroll[1]))
 
 
-	if target.collidepoint(mx, my):
-		if clicking:
-			hitmarker_sound.play()
-			pygame.draw.rect(screen, (255, 255, 0), target)
-		else:
-			pygame.draw.rect(screen, (0, 255, 255), target)
-	else:
-		pygame.draw.rect(screen, (0, 255, 255), target)
+	for enemy in enemies:
+		screen.blit(enemy.image, (enemy.rect.x - scroll[0], enemy.rect.y - scroll[1]))
+		for bullet in bullets:
+			if enemy.rect.colliderect(bullet.rect):
+				hitmarker_sound.play()
+				enemies.remove(enemy)
+				bullets.remove(bullet)
 
 	for bullet in bullets:
-		# note you have to rotate considering that the base bullet img is pointing straight right (0 radians)
-		# screen.blit(pygame.transform.rotate(img, degrees), (x, y))
-		bullet.x += bullet.vel_x 
-		bullet.y += bullet.vel_y
-		pygame.draw.rect(screen, (255, 255, 0), pygame.Rect(bullet.x, bullet.y, 50, 50))
-		# screen.blit(bullet_img, (bullet.x, bullet.y))
+		bullet.count += 1
+		if bullet.count > 144: 
+			bullets.remove(bullet)
+			continue
+		bullet.move()
+		if bullet.flip:		
+			modified_bullet_img = pygame.transform.rotate(pygame.transform.flip(bullet_img, bullet.flip, False), degrees(bullet.direction))
+		else:
+			modified_bullet_img = pygame.transform.rotate(pygame.transform.flip(bullet_img, bullet.flip, False), degrees(-1*bullet.direction))		
+		screen.blit(modified_bullet_img, (bullet.rect.x - scroll[0], bullet.rect.y - scroll[1]))
 
-	loc = [mx, my]
+	mouseloc = [mx, my]
 	img_dimensions = crosshair.get_size()
-	mid_x, mid_y = loc[0] - img_dimensions[0]//2, loc[1] - img_dimensions[1]//2
+	mid_x, mid_y = mouseloc[0] - img_dimensions[0]//2, mouseloc[1] - img_dimensions[1]//2
 	screen.blit(crosshair, (mid_x + offset[0], mid_y + offset[1]))
 
+	if moving_right:
+		player_rect.x += 4
+	if moving_left:
+		player_rect.x -= 4
+	if moving_forward:
+		player_rect.y -= 4
+	if moving_backward:
+		player_rect.y += 4
 
 	for event in pygame.event.get():
 		if event.type == QUIT:
@@ -114,6 +154,23 @@ while True:
 			if event.key == K_ESCAPE:
 				pygame.quit()
 				sys.exit()
+			if event.key == K_a:
+				moving_left = True
+			if event.key == K_d:
+				moving_right = True
+			if event.key == K_w:
+				moving_forward = True
+			if event.key == K_s:
+				moving_backward = True
+		if event.type == KEYUP:
+			if event.key == K_a:
+				moving_left = False
+			if event.key == K_d:
+				moving_right = False
+			if event.key == K_w:
+				moving_forward = False
+			if event.key == K_s:
+				moving_backward = False
 		if event.type == MOUSEBUTTONDOWN:
 			if event.button == 1:
 				gun_sound.play()
@@ -126,6 +183,9 @@ while True:
 	font = pygame.font.SysFont('gillsans', 35)
 	display_direction = font.render(str(player_mouse_direction()), True, pygame.Color('white'))
 	screen.blit(display_direction, (25, 25))
+
+	position = font.render(str(player_rect.x) + ', ' + str(player_rect.y), True, pygame.Color('white'))
+	screen.blit(position, (25, 50))
 
 	pygame.display.update()
 	mainClock.tick(144)
